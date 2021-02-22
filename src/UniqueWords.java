8a21cca0576530a4861d7f59b4312c381a851f8b
RyanTjia19 <78666741+RyanTjia19@users.noreply.github.com>
2021-02-22 15:48:55 -0500

    Changed the file

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -7,22 +7,34 @@ public class UniqueWords
 		@param list ArrayList of strings to be examined
 		@return number of unique strings in the list
    */
-   public static int countUnique(ArrayList<String> list)
-   {
-	  int count = 0;
+   public static int countUnique(ArrayList<String> list) {
+	   
+	   //Changed this variable to the size of the array list
+	   int count = list.size();
 	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
-      }
+	   for (int i = 0; i < list.size(); i++) {
+    	  
+		   /* Changed this for loop so that it will compare the first word with the next word
+		      that is to the right of the first word and not to the left */
+		   /* Ex: First word is blackboard and it will be compared to the second orange not to
+		      to the first orange */
+		   for (int j = i + 1; j < list.size(); j++) {
+			   
+			   /* This is so that if there are words that aren't unique it won't count towards
+			      the final count */
+			   //This can be done by subtracting the final count by one
+			   if (list.get(i).equalsIgnoreCase(list.get(j))) {
+				
+				   count--;
+			   }
+		   }
+	   }
+      
 	  return count;
-   }
+}
 
-   public static void main(String[] args)
-   {
-      ArrayList <String> words = new ArrayList<>();
+   public static void main(String[] args) {
+	  ArrayList <String> words = new ArrayList<>();
       words.add("apple");
 	  words.add("orange");
 	  words.add("blackboard");
